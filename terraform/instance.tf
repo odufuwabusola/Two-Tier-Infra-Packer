30ebd7f9b93ee604be2e4f029fbe6452717fa94d
BUSOLA-O <102264029+BUSOLA-O@users.noreply.github.com>
2026-01-20 23:00:10 +0000

    file updated

---
+++ terraform/instance.tf
@@ -0,0 +1,51 @@
+
+
+resource "aws_vpc" "week5-vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "week5-vpc"
+  }
+}
+
+resource "aws_subnet" "public-week5-subnet" {
+  vpc_id            = aws_vpc.week5-vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "public-week5-subnet"
+  }
+}
+
+resource "aws_subnet" "private-week5-subnet" {
+  vpc_id            = aws_vpc.week5-vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "private-week5-subnet"
+  }
+}
+
+resource "aws_instance" "nginx" {
+  ami           = "resolve:ssm:/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+  instance_type = "t3.micro"
+  subnet_id              = "aws_subnet.public-week5-subnet.id"
+  vpc_security_group_ids = ["aws_security_group.allow_tls.id"]
+  key_name               = "MasterClass2026"
+  tags = {
+    Name = "Nginx"
+  }
+}
+
+resource "aws_instance" "python" {
+  ami           = "resolve:ssm:/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+  instance_type = "t3.micro"
+  subnet_id              = "aws_subnet.private-week5-subnet.id"
+  vpc_security_group_ids = ["aws_security_group.allow_tls.id"]
+  key_name               = "MasterClass2026"
+  tags = {
+    Name = "python"
+  }
+}
---
+++ terraform/route.tf
@@ -0,0 +1,25 @@
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.week5-vpc.id
+
+  tags = {
+    Name = "week5-gw"
+  }
+}
+
+resource "aws_route_table" "week5-rt" {
+  vpc_id = aws_vpc.week5-vpc.id
+
+  route {
+    cidr_block = aws_subnet.public-week5-subnet.cidr_block
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "week5-rt"
+  }
+}
+
+resource "aws_route_table_association" "week5-rt-a" {
+  subnet_id      = aws_subnet.public-week5-subnet.id
+  route_table_id = aws_route_table.week5-rt.id 
+}
