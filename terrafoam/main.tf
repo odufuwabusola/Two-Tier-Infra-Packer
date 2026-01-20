62a3d113c8a7d25da83063c98615d8ce04aadef9
odufuwabusola <odufuwabusola@gmail.com>
2026-01-20 19:12:44 +0000

    Update main.tf

--- terrafoam/main.tf
+++ terrafoam/main.tf
@@ -1,1 +1,25 @@
-# placeholder foor tf
+
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "nginx-node" {
+  ami                    = "ami-07ff62358b87c7116"
+  instance_type          = "t3.micro"
+  subnet_id              = "subnet-02874a91427bf9e53"
+  vpc_security_group_ids = ["sg-0ed1da35b104c139c"]
+  key_name               = "MasterClass2026"
+
+  tags = {
+    Name = "terraform-nginx-node"
+  }
+}
