e0533ab52f70d78de1eef300c0b38129740f5455
BUSOLA-O <102264029+BUSOLA-O@users.noreply.github.com>
2026-01-21 17:00:10 +0000

    main.tf was  updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,8 +16,34 @@ provider "aws" {
 }
 
 
+resource "aws_instance" "java-node" {
+  ami                    = "ami-02aa3c01c7c7a50e5"
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.public-week5-subnet.id
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  key_name               = "MasterClass2026"
+
+  tags = {
+    Name = "terraform-java-node"
+  }
+
+}
+
+resource "aws_instance" "python-node" {
+  ami                    = "ami-036c7b34d4f1fae43"
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.public-week5-subnet.id
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  key_name               = "MasterClass2026"
+
+  tags = {
+    Name = "terraform-python-node"
+  }
+
+}
+
 resource "aws_instance" "nginx-node" {
-  ami                    = "ami-07ff62358b87c7116"
+  ami                    = "ami-008da92ce3b118ee8"
   instance_type          = "t3.micro"
   subnet_id              = aws_subnet.public-week5-subnet.id
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
